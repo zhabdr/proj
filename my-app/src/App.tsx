4ac34cb748f69c0783de2537ded40056cac5eb25
zhabdr <you@example.com>
2023-08-14 05:22:54 +0300

    dadad

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component, useState } from 'react';
+// import count from './component/count';
+import gg from './component/count';
+function App() {
+
+return(
+  <div className='api'>
+    <gg/>
+  </div>
+)
+}
+
+export default App;
---
+++ my-app/src/component/count.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { useState } from "react"
+// import App from "../App"
+// const count=()=>{
+//     const[count,setcount]=useState(0)
+//   const counter=()=>{
+// setcount(count+1)
+//   }
+//   const counter_minus=()=>{
+//     setcount(count-1)
+//       }
+//   return (
+//     <div className="App">
+//    <p>{count}</p>
+//    <button onClick={counter}>+</button>
+//    <button onClick={counter_minus}>-</button>
+//     </div>
+//   );}
+//  export default count
+const gg=()=>{
+  return(
+    <div>
+      <h1>efrfr</h1>
+      
+    </div>
+  )
+}
+export default gg
